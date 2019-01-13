fa8d98f3965765e34510762b57722423afaa8694
Vlad Kharitonov <vlad@wings.ai>
2019-01-13 21:44:59 +0300

    task 2 week 4

--- week4_divide_and_conquer/2_majority_element/majority_element.py
+++ week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -6,13 +6,29 @@ def get_majority_element(a, left, right):
         return -1
     if left + 1 == right:
         return a[left]
-    #write your code here
+
+    left_m = get_majority_element(a, left, (left + right - 1)//2 + 1)
+    right_m = get_majority_element(a, (left + right - 1)//2 + 1, right)
+    left_count = 0
+    for i in range(left, right):
+        if a[i] == left_m:
+            left_count += 1
+    if left_count > (right-left)//2:
+        return left_m
+
+    right_count = 0
+    for i in range(left, right):
+        if a[i] == right_m:
+            right_count += 1
+    if right_count > (right-left)//2:
+        return right_m
+
     return -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if get_majority_element(a, 0, len(a)) != -1:
         print(1)
     else:
         print(0)
